f6c6da1f5c971406fdbc19d7d1355daab5d729ee
Matthew RIley <matthewriley@Matthews-MacBook-Pro.local>
2019-11-06 13:43:42 -0600

    Added image, but still not working. Also, added in calculator component and calculator.css

---
+++ src/components/Calculator/Calculator.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Calculator.css'
+
+// Main calculator component. Contains our other calculator components.
+class Calculator extends Component {
+    render() {
+        return (
+            <div className='Calculator'>
+                <h1>Sup?</h1>
+            </div>
+        );
+    }
+}
+
+export default Calculator;
