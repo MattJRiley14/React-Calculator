77e4487f304aafb4009fc3a8c0120e303419d849
Matthew RIley <matthewriley@Matthews-MacBook-Pro.local>
2019-11-07 12:37:02 -0600

    Added in ComputationScreen component, styled it, and rendered it in the Screen component

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import './Calculator.css'
+import Screen from '../Screen/Screen';
 
 // Main calculator component. Contains our other calculator components.
 class Calculator extends Component {
     render() {
         return (
             <div className='Calculator'>
-                <h1>Sup?</h1>
+                <Screen />
             </div>
         );
     }
